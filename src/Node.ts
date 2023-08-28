0764fb1cd02a66fb512f77a282a5cfff48527376
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-08-28 10:36:24 +0200

    Nodes can have children

--- src/Node.ts
+++ src/Node.ts
@@ -2,10 +2,17 @@ import Type from './Type'
 
 export default class Node {
     type: Type
-    value: string
+    value: string | number
+    children: Node[]
 
-    constructor(type: Type, value: string) {
+    constructor(type: Type, value: string | number) {
         this.type = type
         this.value = value
+        this.children = []
+    }
+
+    addChild(child: Node) {
+        this.children.push(child)
+        return child
     }
 }
--- tests/node.test.ts
+++ tests/node.test.ts
@@ -6,3 +6,16 @@ it('can instantiate a new node', () => {
     expect(node.type).toBe(Type.Operator)
     expect(node.value).toBe('+')
 })
+
+it('has zero children', () => {
+    let node = new Node(Type.Operator, '+')
+    expect(node.children).toEqual([])
+})
+
+it('can add a child', () => {
+    let parent = new Node(Type.Operator, '+')
+    let child = new Node(Type.Operator, 3)
+    expect(parent.addChild(child)).toBe(child)
+    expect(parent.children).toEqual([child])
+    expect(parent.children[0]).toBe(child)
+})
