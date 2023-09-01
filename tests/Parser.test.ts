9a7cacd3560f815a9d55eadf885185142ed3ccd9
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-01 09:41:26 +0200

    It can parse expressions with three operators

--- tests/Parser.test.ts
+++ tests/Parser.test.ts
@@ -40,26 +40,92 @@ it('can parse an expression with two operators #2', () => {
     expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #1', () => {
+it('can parse an expression with three operators #1', () => {
     let input = new Tokenizer().tokenize('2 + 3 * 4 ^ 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    root.addChild(new Node(Type.Number, 2))
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    times.addChild(new Node(Type.Number, 3))
+    let power = times.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 4))
+    power.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #2', () => {
+it('can parse an expression with three operators #2', () => {
     let input = new Tokenizer().tokenize('2 + 3 ^ 4 * 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    root.addChild(new Node(Type.Number, 2))
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    let power = times.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 3))
+    power.addChild(new Node(Type.Number, 4))
+    times.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #3', () => {
+it('can parse an expression with three operators #3', () => {
     let input = new Tokenizer().tokenize('2 * 3 + 4 ^ 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    times.addChild(new Node(Type.Number, 2))
+    times.addChild(new Node(Type.Number, 3))
+    let power = root.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 4))
+    power.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #4', () => {
+it('can parse an expression with three operators #4', () => {
     let input = new Tokenizer().tokenize('2 * 3 ^ 4 + 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    times.addChild(new Node(Type.Number, 2))
+    let power = times.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 3))
+    power.addChild(new Node(Type.Number, 4))
+    root.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #5', () => {
+it('can parse an expression with three operators #5', () => {
     let input = new Tokenizer().tokenize('2 ^ 3 + 4 * 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    let power = root.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 2))
+    power.addChild(new Node(Type.Number, 3))
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    times.addChild(new Node(Type.Number, 4))
+    times.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
 
-it.skip('can parse an expression with three operators #6', () => {
+it('can parse an expression with three operators #6', () => {
     let input = new Tokenizer().tokenize('2 ^ 3 * 4 + 5')
+    let result = new Parser().parse(input)
+
+    let root = new Node(Type.Operator, '+')
+    let times = root.addChild(new Node(Type.Operator, '*'))
+    let power = times.addChild(new Node(Type.Operator, '^'))
+    power.addChild(new Node(Type.Number, 2))
+    power.addChild(new Node(Type.Number, 3))
+    times.addChild(new Node(Type.Number, 4))
+    root.addChild(new Node(Type.Number, 5))
+
+    expect(result).toEqual(root)
 })
