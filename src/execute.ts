8ebe0bf0f4f564e52d0d974deb971cbd47e2da77
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-13 21:19:29 +0200

    It can match expressions

--- src/execute.ts
+++ src/execute.ts
@@ -1,5 +1,6 @@
 import Action from './action'
 import Node from './node'
+import string from './output/string'
 import parse from './parse'
 import Type from './type'
 import Variables from './variables'
@@ -22,15 +23,19 @@ function findVariables(action: Action, node: Node): Variables | null {
         }
     }
 
-    let numbericVariables = findNumericVariables(action, node)
-    if (!numbericVariables) return null
-
-    return { ...numbericVariables }
-}
-
-function findNumericVariables(action: Action, node: Node): Variables | null {
     let variables: Variables = {}
 
+    if (!findNumericVariables(action, node, variables)) return null
+    if (!findExpressions(action, node, variables)) return null
+
+    Object.keys(variables).forEach(
+        key => (variables[key] = variables[key].constructor?.name === 'Node' ? string(variables[key]) : variables[key])
+    )
+
+    return variables
+}
+
+function findNumericVariables(action: Action, node: Node, variables: Variables): boolean {
     let numbers = node.children.filter(child => child.type === Type.Number)
 
     let numbericVariables = Object.entries(action.variables)
@@ -39,9 +44,25 @@ function findNumericVariables(action: Action, node: Node): Variables | null {
 
     for (let index = 0; index < numbericVariables.length; index++) {
         let number = numbers[index]
-        if (!number) return null
+        if (!number) return false
         variables[numbericVariables[index]] = Number(number.value)
     }
 
-    return variables
+    return true
+}
+
+function findExpressions(action: Action, node: Node, variables: Variables): boolean {
+    let expressions = node.children.filter(expression => !Object.values(variables).includes(expression))
+
+    let expressionVariables = Object.entries(action.variables)
+        .filter(variable => variable[1] === 'expression')
+        .map(variable => variable[0])
+
+    for (let index = 0; index < expressionVariables.length; index++) {
+        let expression = expressions[index]
+        if (!expression) return false
+        variables[expressionVariables[index]] = expression
+    }
+
+    return true
 }
--- src/variables.ts
+++ src/variables.ts
@@ -1,3 +1,3 @@
 export default interface Variables {
-    [key: string]: string | number
+    [key: string]: any
 }
