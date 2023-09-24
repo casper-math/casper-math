d6f6d9b7272dd5b08f3e56e47ec762f353a128b4
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-24 14:26:04 +0200

    Refactor

--- src/actions/arithmetic.ts
+++ src/actions/arithmetic.ts
@@ -3,24 +3,24 @@ import { Action } from '../interfaces'
 
 const add: Action = {
     name: 'add numbers',
-    pattern: 'x + y',
-    variables: { x: 'number', y: 'number' },
-    handle: ({ x, y }) => Number(x) + Number(y)
+    pattern: 'a + b',
+    variables: { a: 'number', b: 'number' },
+    handle: ({ a, b }) => Number(a) + Number(b)
 }
 
 const multiply: Action = {
     name: 'multiply numbers',
-    pattern: 'x * y',
-    variables: { x: 'number', y: 'number' },
-    handle: ({ x, y }) => Number(x) * Number(y)
+    pattern: 'a * b',
+    variables: { a: 'number', b: 'number' },
+    handle: ({ a, b }) => Number(a) * Number(b)
 }
 
 const simplify: Action = {
     name: 'simplify fractions',
-    pattern: 'x / y',
-    variables: { x: 'number', y: 'number' },
-    handle: ({ x, y }) => {
-        return new Fraction(Number(x), Number(y)).toString()
+    pattern: 'a / b',
+    variables: { a: 'number', b: 'number' },
+    handle: ({ a, b }) => {
+        return new Fraction(Number(a), Number(b)).toString()
     }
 }
 
@@ -28,18 +28,15 @@ const addFractions: Action = {
     name: 'add fractions',
     pattern: 'a / b + c / d',
     variables: { a: 'number', b: 'number', c: 'number', d: 'number' },
-    handle: ({ a, b, c, d }) => {
-        return Fraction.add(new Fraction(Number(a), Number(b)), new Fraction(Number(c), Number(d))).toString()
-    }
+    handle: ({ a, b, c, d }) =>
+        Fraction.add(new Fraction(Number(a), Number(b)), new Fraction(Number(c), Number(d))).toString()
 }
 
 const addFractionsAndNumbers: Action = {
     name: 'add fractions and numbers',
     pattern: 'a / b + c',
     variables: { a: 'number', b: 'number', c: 'number' },
-    handle: ({ a, b, c }) => {
-        return Fraction.add(new Fraction(Number(a), Number(b)), Number(c)).toString()
-    }
+    handle: ({ a, b, c }) => Fraction.add(new Fraction(Number(a), Number(b)), Number(c)).toString()
 }
 
 export default [add, multiply, simplify, addFractions, addFractionsAndNumbers]
