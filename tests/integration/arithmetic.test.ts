8b71b3c3008adb0f4d491b30a39a2fd373b41898
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-24 14:24:01 +0200

    Add tests

--- tests/integration/arithmetic.test.ts
+++ tests/integration/arithmetic.test.ts
@@ -80,7 +80,10 @@ it.each([
     ['24 / 42', '4 / 7'],
     ['27 / 15', '9 / 5'],
     ['32 / 12', '8 / 3'],
-    ['1 / 2 + 1 / 3', '5 / 6']
+    ['1 / 2 + 1 / 3', '5 / 6'],
+    ['1 / 2 + 1 / 4', '3 / 4'],
+    ['3 / 4 - 1 / 3', '5 / 12'],
+    ['(1 + 1 / 2) - 1 / 4', '5 / 4']
 ])('can do arithmetic with fractions', (input: string, output: string) => {
     let result = casper().go(input)
     expect(result.result).toBe(output)
