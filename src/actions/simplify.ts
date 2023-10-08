6c7ec65ec3e1bd4675d5727ce32efe0c191652f5
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-10-08 11:51:48 +0200

    It can add like terms and simplify products

--- src/actions/simplify.ts
+++ src/actions/simplify.ts
@@ -1,24 +1,79 @@
-import { Action } from '../interfaces'
-
-const addOneAndOne: Action = {
-    name: 'add one and one',
-    pattern: 'a + a',
-    variables: { a: 'expression' },
-    handle: ({ a }) => `2 * (${a})`
-}
+import Fraction from '../fraction'
+import { Action, Type } from '../interfaces'
+import Node from '../node'
+import string from '../output/string'
+import parse from '../parse'
 
 const addLikeTerms: Action = {
     name: 'add like terms',
-    pattern: 'a * x + b * x',
-    variables: { a: 'number', b: 'number', x: 'expression' },
-    handle: ({ a, b, x }) => `${Number(a) + Number(b)} * (${x})`
+    run(node) {
+        if (node.value !== '+') return node
+
+        const coefficients: { [key: string]: Fraction } = {}
+
+        function push(key: string | Node, value: number | Fraction = 1) {
+            key = typeof key === 'string' ? key : string(key)
+            value = typeof value === 'number' ? new Fraction(value, 1) : value
+
+            if (key in coefficients) {
+                coefficients[key] = Fraction.add(coefficients[key], value)
+            } else {
+                coefficients[key] = value
+            }
+        }
+
+        node.children.forEach(child => {
+            if (child.type === Type.Operator && child.value === '*') {
+                const numbers = child.children.filter(factor => factor.type === Type.Number)
+
+                if (numbers.length === 0) {
+                    push(child, 1)
+                    node.removeChild(child)
+                    return
+                }
+
+                if (numbers.length === 1) {
+                    child.removeChild(numbers[0])
+                    push(child, Number(numbers[0].value))
+                    node.removeChild(child)
+                    return
+                }
+
+                return
+            }
+
+            node.removeChild(child)
+            push(child, 1)
+            return
+        })
+
+        Object.keys(coefficients).forEach(factor => {
+            node.addChild(parse(`${coefficients[factor].toString()} * (${factor})`))
+        })
+
+        return node
+    }
 }
 
-const addOne: Action = {
-    name: 'add one',
-    pattern: 'a * x + x',
-    variables: { a: 'number', x: 'expression' },
-    handle: ({ a, x }) => `${Number(a) + 1} * (${x})`
+const multiplyByZero: Action = {
+    name: 'multiply by zero',
+    pattern: 'x * 0',
+    variables: { x: 'expression' },
+    handle: () => 0
 }
 
-export default [addOneAndOne, addLikeTerms, addOne]
+const multiplyByOne: Action = {
+    name: 'multiply by one',
+    pattern: 'x * 1',
+    variables: { x: 'expression' },
+    handle: ({ x }) => x
+}
+
+const square: Action = {
+    name: 'square',
+    pattern: 'x * x',
+    variables: { x: 'expression' },
+    handle: ({ x }) => `(${x}) ^ 2`
+}
+
+export default [addLikeTerms, multiplyByZero, multiplyByOne, square]
--- tests/integration/simplify.test.ts
+++ tests/integration/simplify.test.ts
@@ -1,4 +1,4 @@
-import casper from '../../src/index'
+import casper from './../../src/index'
 
 it.each([
     ['p + p + p + p', '4 * p'],
@@ -6,7 +6,23 @@ it.each([
     ['a + a + a + a + a + a + a + a', '8 * a'],
     ['r + r + r + r + r + r + r', '7 * r'],
     ['5a * 9b', 'a * b * 45'],
-    ['25a * -4b', 'a * b * -100']
+    ['25a * -4b', 'a * b * -100'],
+    ['-8b * 4a', 'b * a * -32'],
+    ['-3p * -q', 'p * q * 3'],
+    ['6x * -5', 'x * -30'],
+    ['-p * -a', 'a * p'],
+    ['0.2a * c * 5b', 'c * b * a'],
+    ['-8x * -y * 3z', 'x * y * z * 24'],
+    ['-a * -b * -1', 'b * -1 * a'],
+    ['5y * -3x * -z', 'y * x * z * 15'],
+    ['18y * 0 * -2x', '0'],
+    ['-0.5a * c * -8b', 'a * c * b * 4'],
+    ['15a * 3a', '45 * a ^ 2'],
+    ['15a * 3b', 'a * b * 45'],
+    ['(-1 / 3) * x * (-1 / 2) * x', '(1 / 6) * x ^ 2'],
+    ['(-2 / 3) * (-3 / 4) * y', 'y * (1 / 2)'],
+    ['(1 / 3) * x * (2 / 3) * y * (-3 / 5) * x', 'y * x ^ 2 * (-2 / 15)'],
+    ['(2 / 3) * x * (1 / 2) * y * 3z', 'y * z * x']
 ])('simplifies the expression', (input: string, output: string) => {
     const result = casper().go(input)
     expect(result.result).toBe(output)
